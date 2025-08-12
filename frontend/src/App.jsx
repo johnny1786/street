4ce5d28f856b0b0ec63c180d3c875a4e9a856cad
johnny1786 <jovialjohn4054@gmail.com>
2025-08-12 21:49:27 +0530

    Home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
@@ -12,6 +13,7 @@ export default function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Welcome to My App</h1>
+      <p>This is the landing page of your website.</p>
+    </div>
+  );
+}

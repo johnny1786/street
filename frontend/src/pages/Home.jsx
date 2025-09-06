bc5667221efc9da7d5948f07449013fb09961de0
johnny1786 <jovialjohn4054@gmail.com>
2025-09-07 01:04:50 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,13 @@
+
+
 export default function Home() {
   return (
     <div style={{ padding: "2rem" }}>
       <h1>Welcome to My App</h1>
-      <p>This is the landing page of your website.</p>
+      <p className="mt-4 text-green-500 text-xl text-center">This is the landing page of your website.</p>
+      <button className="mt-8 px-6 py-3 bg-white text-green-500 font-semibold rounded-lg shadow-md hover:bg-gray-100 transition duration-300">
+              Get Started
+            </button>
     </div>
   );
 }
